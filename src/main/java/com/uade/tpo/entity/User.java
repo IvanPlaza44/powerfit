e864a3ba852ca7cca7faf6826c5781da41b08d09
IvanPlaza44 <plazaivan44@gmail.com>
2026-04-11 18:39:17 -0300

    Tabla users por user

--- src/main/java/com/uade/tpo/entity/User.java
+++ src/main/java/com/uade/tpo/entity/User.java
@@ -5,14 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
